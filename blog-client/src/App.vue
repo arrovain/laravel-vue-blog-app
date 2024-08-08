0463655995fe8c3a73e5224220c1324b6c7df780
Mahmut İÇME <arrovain@gmail.com>
2024-08-08 14:38:05 +0300

    axios.post

--- blog-client/src/App.vue
+++ blog-client/src/App.vue
@@ -2,9 +2,14 @@
 import axios from 'axios';
 
 
- axios.get('/sanctum/csrf-cookie').then(response => {
+ axios.get('http://127.0.0.1:8000/sanctum/csrf-cookie').then(response => {
 
  });
+
+ axios.post('http://127.0.0.1:8000/login', {
+  email:'arrovain@gmail.com',
+  password: 'password'
+ })
 </script>
 
 <template>
