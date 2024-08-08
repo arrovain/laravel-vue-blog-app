7bd152f64f1f8ff6b1ba13b4832f24c301f94385
Mahmut İÇME <arrovain@gmail.com>
2024-08-08 14:27:19 +0300

    axios defaults (true)

--- blog-client/src/main.js
+++ blog-client/src/main.js
@@ -5,7 +5,10 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
 
+axios.defaults.withCredentials= true;
+axios.defaults.withXSRFToken = true;
 const app = createApp(App)
 
 app.use(createPinia())
