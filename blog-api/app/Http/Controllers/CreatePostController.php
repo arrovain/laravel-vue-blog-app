7bc17c01fcf42ea9f18d66f2be8560019d4adc29
Mahmut İÇME <arrovain@gmail.com>
2024-09-06 11:13:42 +0300

    invoke method, which allows the controller to be called as a function, takes a CreatePostRequest object as a parameter and after creating the post, the method returns a JSON response

--- blog-api/app/Http/Controllers/CreatePostController.php
+++ blog-api/app/Http/Controllers/CreatePostController.php
@@ -2,15 +2,21 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\CreatePostRequest;
+use App\Models\Post;
+
+
 
 class CreatePostController extends Controller
 {
     /**
      * Handle the incoming request.
      */
-    public function __invoke(Request $request)
+    public function __invoke(CreatePostRequest $request)
     {
-        //
+        Post::create($request->validate());
+
+        return response()->json(['message' => 'Post created successfully'], 201);
     }
 }
+ 
