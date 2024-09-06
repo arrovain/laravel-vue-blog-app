04b2ee63cc37bc93df1519d22ac9408f94ad1b57
Mahmut İÇME <arrovain@gmail.com>
2024-09-06 11:09:42 +0300

    authorize true and rules for title and body

--- blog-api/database/migrations/2024_09_06_073941_create_posts_table.php
+++ blog-api/database/migrations/2024_09_06_073941_create_posts_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
             $table->string('title');
             $table->string('slug')->unique();
             $table->text('body');
