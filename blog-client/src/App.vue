404191fbfe82727774dad646e0ea68fff6715ae3
Mahmut İÇME <arrovain@gmail.com>
2024-09-06 09:33:47 +0300

    login path

--- blog-client/src/App.vue
+++ blog-client/src/App.vue
@@ -9,7 +9,13 @@ await axios.get('http://localhost:8000/sanctum/csrf-cookie').then(response => {
  await axios.post('http://localhost/login', {
   email:'arrovain@gmail.com',
   password: 'password'
- }) }
+ })
+ 
+ await axios.get('http://localhost:8000/api/user').then(response => {
+    console.log(response);
+ });
+ 
+ }
 </script>
 
 <template>
