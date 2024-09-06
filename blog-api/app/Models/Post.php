70e4270177083c3c4195e007769a7913cf89f06f
Mahmut İÇME <arrovain@gmail.com>
2024-09-06 10:51:31 +0300

    a callback function that will be executed before a post is saved and Str converts a string into a URL-friendly slug

--- blog-api/app/Models/Post.php
+++ blog-api/app/Models/Post.php
@@ -4,10 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Post extends Model
 {
     use HasFactory;
 
     protected $guarded = [];
+
+    public static function booted() {
+        static::saving(function ($post) {
+            $post->slug = Str::slug($post->title);
+        });
+    }
 }
